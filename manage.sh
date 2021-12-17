23ec07ea02278d1bd983f32fccd09371596c5a67
root <root@localhost.localdomain>
2021-12-17 22:57:18 +0300

    1

---
+++ manage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function warn() {
+    echo "WARNING: This script is deprecated!"
+    echo "Please use the Makefile instead"
+}
+
+function up() {
+    warn()
+    docker-compose up -d
+}
+
+function setup() {
+    warn()
+	mkdir -p web/db
+    docker-compose up -d --force-recreate && docker-compose exec web ./manage.py createsuperuser
+}
+
+function stop() {
+    warn()
+    docker-compose stop
+}
+
+function clean() {
+    docker-compose down
+    docker-compose rm
+}
+
+function redo() {
+    clean
+    setup
+}
+
+function cleardb() {
+	rm -rf web/db
+}
+
+function fresh() {
+	clean
+	cleardb
+	setup
+}
+
+$@
